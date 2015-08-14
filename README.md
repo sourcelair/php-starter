44beb43be69798f07e7a31d9eecf9995c31bf417
Paris <paris@sourcelair.com>
2015-08-14 13:30:11 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PHP template
+
+Stripped-down template to get started with PHP.
+
+https://www.sourcelair.com/blueprints/sourcelair/php-template
---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf8" />
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="main.css" />
+	</head>
+    <body>
+        <h1>Hello</h1>
+        <?php echo '<p>Welcome to your new PHP project, at SourceLair.</p>'; ?>
+	</body>
+</html>
